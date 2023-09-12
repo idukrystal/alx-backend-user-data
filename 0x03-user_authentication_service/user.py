c1d033890e49c57a4690658fba0c62b4db5b0655
Crysart <idukrystal@gmail.com>
2023-09-12 22:29:50 +0100

    00 t4

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -14,7 +14,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    email = Column(String)
-    hashed_password = Column(String)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
     session_id = Column(String, nullable=True)
     reset_token = Column(String, nullable=True)
