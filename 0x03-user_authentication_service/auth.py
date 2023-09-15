0880753d444fb57d3741bdbedcdfc4a24a30ac99
Crysart <idukrystal@gmail.com>
2023-09-15 09:22:40 +0100

    test 08 2

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -44,14 +44,3 @@ class Auth:
             return False
         except NoResultFound:
             return False
-
-    def create_session(self, email: str) -> Union[str, None]:
-        """ regusters a users session id """
-        try:
-            user = self._db.find_user_by(email=email)
-            uid = _generate_uuid()
-            user.session_id = uid
-            #self._db._session.commit()
-            return uid
-        except NoResultFound:
-            return None
