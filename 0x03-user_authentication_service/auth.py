257b58ffad1e7791867f4a49039202c73c031a45
Crysart <idukrystal@gmail.com>
2023-09-15 21:21:37 +0100

    pip

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -66,7 +66,7 @@ class Auth:
     def destroy_session(user_id: int) -> None:
         """ Deletes a user session uuid """
         try:
-            user = self._db.find_user_by(id=user_id)
             self.db.update_user(user_id, session_id=None)
         except ValueError:
             return None
+        return None
