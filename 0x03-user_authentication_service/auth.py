c1494f806246b81c2c4c47431bb3c2d35cf48783
Crysart <idukrystal@gmail.com>
2023-09-15 21:23:42 +0100

    you

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -63,10 +63,9 @@ class Auth:
         except NoResultFound:
             return None
 
-    def destroy_session(user_id: int) -> None:
+    def destroy_session(self, user_id: int) -> None:
         """ Deletes a user session uuid """
         try:
             self.db.update_user(user_id, session_id=None)
         except ValueError:
-            return None
-        return None
+            return
