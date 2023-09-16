85fb3e5a27976eac96b6a2dc37762a9cb8a21afe
Crysart <idukrystal@gmail.com>
2023-09-16 14:05:44 +0100

    96

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,21 +1,5 @@
 #!/usr/bin/env python3
 """
-Main file
+Main file: test all functions integration
 """
-from user import User
-from db import DB
 
-
-print(User.__tablename__)
-
-for column in User.__table__.columns:
-    print("{}: {}".format(column, column.type))
-
-
-my_db = DB()
-
-user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
-print(user_1.id)
-
-user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
-print(user_2.id)
